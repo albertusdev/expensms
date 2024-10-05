3f74cb2d40b5b2558eb20c37388ccd2b492875a5
Albertus Angga Raharja <hello@albertus.dev>
2024-10-05 12:58:07 +0700

    fix: regression in permission request for fresh user

--- app/src/main/java/dev/albertus/expensms/MainActivity.kt
+++ app/src/main/java/dev/albertus/expensms/MainActivity.kt
@@ -1,8 +1,11 @@
 package dev.albertus.expensms
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
@@ -19,15 +22,20 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.albertus.expensms.ui.screens.ErrorScreen
 import dev.albertus.expensms.ui.screens.MainScreen
+import dev.albertus.expensms.ui.screens.PermissionScreen
 import dev.albertus.expensms.ui.screens.SettingsScreen
 import dev.albertus.expensms.ui.screens.SmsDetailScreen
 import dev.albertus.expensms.ui.screens.TrashBinScreen
@@ -37,12 +45,24 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var permissionState: MutableState<Boolean>
     private val viewModel: MainViewModel by viewModels()
+
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        permissionState.value = isGranted
+        if (isGranted) {
+            viewModel.loadSmsMessages()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ExpenSMSTheme {
+                permissionState = remember { mutableStateOf(checkPermission()) }
                 val navController = rememberNavController()
                 val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
                 val scope = rememberCoroutineScope()
@@ -91,7 +111,17 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 ) {
-                    NavHost(navController, startDestination = "main") {
+                    NavHost(
+                        navController,
+                        startDestination = if (permissionState.value) "main" else "permission",
+                    ) {
+                        composable("permission") {
+                            PermissionScreen(
+                                onRequestPermission = {
+                                    requestSmsPermission()
+                                }
+                            )
+                        }
                         composable(
                             route = "main",
                             enterTransition = {
@@ -190,7 +220,6 @@ class MainActivity : ComponentActivity() {
                             if (transaction != null) {
                                 SmsDetailScreen(
                                     transaction = transaction,
-                                    rawSms = viewModel.getRawSmsForTransaction(transactionId),
                                     onNavigateBack = { navController.popBackStack() }
                                 )
                             } else {
@@ -236,5 +265,16 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_SMS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestSmsPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.READ_SMS)
     }
 }
